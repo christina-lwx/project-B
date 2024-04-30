ae81d4ae26f3b9d9f764ed51026995b6ce28745f
christina-lwx <157680394+christina-lwx@users.noreply.github.com>
2024-04-30 14:21:10 +0800

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,12 @@
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent("canvasContainer");
+  background(255);
+}
+
+function draw() {
+  textAlign(CENTER,CENTER);
+  textSize(50);
+  text("depression",width/2,height/2);
+
+}
