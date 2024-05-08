ade6331a3621541216b5955d4a9cf36cf4f8dbc1
christina-lwx <wl2917@nyu.edu>
2024-05-08 19:33:49 +0800

    a lot

--- js/script.js
+++ js/script.js
@@ -1,55 +1,46 @@
-let backIamge;
+let backImage;
 let video;
 let isPlay = false;
 let song;
 let font;
-let nextButton
+let nextButton;
 
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("canvasContainer");
-    backIamge = loadImage("img/backPage1.png")
+    backImage = loadImage("img/backPage1.png");
     video = createVideo("video/pg1.mp4");
     song = loadSound("music/cat.wav");
     video.hide();
     font = loadFont("font/gingercat-pgg21.ttf");
     nextButton = {
-        x:
-            width  - width/10-50, y:
-            height / 2 , w:
-            width/10, h:
-            50, text:
-            "Next",
-        font:
-        font, text_size:
-            30,
-        color:
-            'rgb(255,255,255)',
-        border_color:
-            '#FFDAB9',
-        //shadow effect when button is hovered over
-        hover_shadow:
-            {
-                blur:
-                    10, color:
-                    'rgb(128,0,0)', offset_x:
-                    1, offset_y:
-                    4
-            }
-        ,
-        corners:
-            [10, 10, 10, 10]
+        x: (width - width / 10 - 50),
+        y: height / 2,
+        w: width / 10,
+        h: 50,
+        text: "Next",
+        font: font,
+        text_size: 30,
+        color: 'rgb(255,255,255)',
+        border_color: '#FFDAB9',
+        hover_shadow: {
+            blur: 10,
+            color: 'rgb(128,0,0)',
+            offset_x: 1,
+            offset_y: 4
+        },
+        corners: [10, 10, 10, 10],
+        onClick: function () {
+            window.location.href = "page2.html";
+        }
     };
     background(255);
 }
 
 function draw() {
-
-    image(backIamge, width / 2, height / 2, windowWidth, windowHeight + 100);
-
+    image(backImage, width / 2, height / 2, windowWidth, windowHeight + 100);
     rectMode(CENTER);
     imageMode(CENTER);
-
     fill(255);
     rect(width / 2, height / 2, 800, 450);
     if (isPlay) {
@@ -61,26 +52,44 @@ function draw() {
     fill(0);
     rectMode(CORNER);
     button(nextButton);
-
-
 }
 
 function mousePressed() {
-    if (mouseX >= width / 2 + width / 6 - 400 && mouseX <= width / 2 + width / 6 &&
-        mouseY >= height - height / 4 && mouseY <= height
+    if (
+        mouseX >= width / 2 + width / 6 - 400 &&
+        mouseX <= width / 2 + width / 6 &&
+        mouseY >= height - height / 4 &&
+        mouseY <= height
     ) {
         isPlay = true;
         video.time(0);
         song.play();
         video.play();
     }
-
-    nextButton.on_clicked = function () {
-        window.location.href="page2.html";
+    if (
+        mouseX >= nextButton.x - nextButton.w / 2 &&
+        mouseX <= nextButton.x + nextButton.w / 2 &&
+        mouseY >= nextButton.y - nextButton.h / 2 &&
+        mouseY <= nextButton.y + nextButton.h / 2
+    ) {
+        nextButton.onClick();
     }
 }
 
 function windowResized() {
-
     resizeCanvas(windowWidth, windowHeight);
+}
+
+function button(btn) {
+    fill(btn.color);
+    stroke(btn.border_color);
+    strokeWeight(2);
+    rect(btn.x, btn.y, btn.w, btn.h, ...btn.corners);
+    fill(0);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    textSize(btn.text_size);
+    textFont(btn.font);
+    // Adjusting text position based on button dimensions and text size
+    text(btn.text, btn.x + btn.w / 2, btn.y + btn.h / 2); // Centering text wit
 }
